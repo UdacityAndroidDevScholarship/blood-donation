f8bd77d77534b0ca86ccd79222143a0eb2616c7a
Bloody-Badboy <arpan.asr30@gmail.com>
2018-04-11 22:14:39 +0530

    Format code using AndroidStyle

--- app/src/main/java/com/udacity/nanodegree/blooddonation/storage/SharedPreferenceManager.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/storage/SharedPreferenceManager.java
@@ -2,6 +2,7 @@ package com.udacity.nanodegree.blooddonation.storage;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+
 import com.udacity.nanodegree.blooddonation.App;
 
 /**
@@ -9,36 +10,36 @@ import com.udacity.nanodegree.blooddonation.App;
  */
 public final class SharedPreferenceManager {
 
-  private final String NAME = "com.udacity.nanodegree.blooddonation";
+    private static SharedPreferences mSharedPreferences;
+    private final String NAME = "com.udacity.nanodegree.blooddonation";
 
-  private static SharedPreferences mSharedPreferences;
+    private SharedPreferenceManager() {
+        mSharedPreferences =
+                App.getInstance().getApplicationContext().getSharedPreferences(NAME,
+                        Context.MODE_PRIVATE);
+    }
 
-  private SharedPreferenceManager() {
-    mSharedPreferences =
-        App.getInstance().getApplicationContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
-  }
+    public static void put(String key, String value) {
+        mSharedPreferences.edit().putString(key, value).apply();
+    }
 
-  public static void put(String key, String value) {
-    mSharedPreferences.edit().putString(key, value).apply();
-  }
+    public static void put(String key, int value) {
+        mSharedPreferences.edit().putInt(key, value).apply();
+    }
 
-  public static void put(String key, int value) {
-    mSharedPreferences.edit().putInt(key, value).apply();
-  }
+    public static void put(String key, boolean value) {
+        mSharedPreferences.edit().putBoolean(key, value).apply();
+    }
 
-  public static void put(String key, boolean value) {
-    mSharedPreferences.edit().putBoolean(key, value).apply();
-  }
+    public static String getString(String key) {
+        return mSharedPreferences.getString(key, "");
+    }
 
-  public static String getString(String key) {
-    return mSharedPreferences.getString(key, "");
-  }
+    public static int getInt(String key) {
+        return mSharedPreferences.getInt(key, -1);
+    }
 
-  public static int getInt(String key) {
-    return mSharedPreferences.getInt(key, -1);
-  }
-
-  public static Boolean getBoolean(String key) {
-    return mSharedPreferences.getBoolean(key, false);
-  }
+    public static Boolean getBoolean(String key) {
+        return mSharedPreferences.getBoolean(key, false);
+    }
 }
