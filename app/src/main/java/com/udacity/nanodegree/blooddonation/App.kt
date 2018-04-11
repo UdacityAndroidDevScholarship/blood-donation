f0f99777643d073bf3a6c0c1024929cd19087fac
Ritesh K Singh <riteshdvps10@gmail.com>
2018-04-11 11:57:09 +0530

    added firebase auth dependency and leak canary

--- app/src/main/java/com/udacity/nanodegree/blooddonation/App.kt
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/App.kt
@@ -1,6 +1,7 @@
 package com.udacity.nanodegree.blooddonation
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by riteshksingh on Apr, 2018
@@ -15,5 +16,13 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        initLeakCanary()
+    }
+
+    private fun initLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return
+
+        LeakCanary.install(this)
     }
 }
