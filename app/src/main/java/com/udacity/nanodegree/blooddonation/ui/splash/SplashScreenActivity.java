fe7b828c1409ad248f01a956459e621a4c47978c
shachi <shachi.shah@einfochips.com>
2018-04-12 12:12:43 +0530

    Made splash screen

--- app/src/main/java/com/udacity/nanodegree/blooddonation/ui/splash/SplashScreenActivity.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/ui/splash/SplashScreenActivity.java
@@ -1,9 +1,16 @@
 package com.udacity.nanodegree.blooddonation.ui.splash;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import com.udacity.nanodegree.blooddonation.R;
 import com.udacity.nanodegree.blooddonation.base.BaseActivity;
 import com.udacity.nanodegree.blooddonation.ui.home.HomeActivity;
@@ -12,29 +19,62 @@ import com.udacity.nanodegree.blooddonation.ui.home.HomeActivity;
  * Created by riteshksingh on Apr, 2018
  */
 public class SplashScreenActivity extends BaseActivity {
-  private Long SPLASH_DELAY = 1000L;
-  private Handler mDelayHandler = null;
+    private Long SPLASH_DELAY = 3000L;
+    private Handler mDelayHandler = null;
+    LinearLayout mlayoutMain;
+    ImageView mImgViewLogo;
 
-  private Runnable runnable = new Runnable() {
-    @Override public void run() {
-      if (!isFinishing()) {
-        Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-        startActivity(intent);
-      }
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
     }
-  };
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash_screen);
+    private Runnable runnable = new Runnable() {
+        @Override public void run() {
+            if (!isFinishing()) {
+                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
 
-    mDelayHandler = new Handler();
 
-    mDelayHandler.postDelayed(runnable, SPLASH_DELAY);
-  }
 
-  @Override protected void onDestroy() {
-    super.onDestroy();
-    mDelayHandler.removeCallbacks(runnable);
-  }
+
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        StartAnimations();
+
+        mDelayHandler = new Handler();
+
+        mDelayHandler.postDelayed(runnable, SPLASH_DELAY);
+    }
+
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        mlayoutMain=(LinearLayout) findViewById(R.id.lin_lay);
+        mlayoutMain.clearAnimation();
+        mlayoutMain.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.trans_anim);
+        anim.reset();
+        mImgViewLogo = (ImageView) findViewById(R.id.image_view_logo);
+        mImgViewLogo.clearAnimation();
+        mImgViewLogo.startAnimation(anim);
+
+    }
+
+
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mDelayHandler.removeCallbacks(runnable);
+    }
 }
