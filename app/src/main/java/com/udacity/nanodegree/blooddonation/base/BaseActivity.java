f3f8b9fcdd24d145a0eb5191652b35402bf38313
Ritesh K Singh <riteshdvps10@gmail.com>
2018-04-13 14:06:53 +0530

    login page databinding

--- app/src/main/java/com/udacity/nanodegree/blooddonation/base/BaseActivity.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/base/BaseActivity.java
@@ -1,9 +1,26 @@
 package com.udacity.nanodegree.blooddonation.base;
 
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by riteshksingh on Apr, 2018
  */
 public class BaseActivity extends AppCompatActivity {
+
+    protected ViewDataBinding mBinding = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mBinding = null;
+        super.onDestroy();
+    }
+
 }
