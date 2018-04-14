feffec5c85166ddfb9d2eaa2e82cfdaf29f7ca3c
Ritesh K Singh <riteshdvps10@gmail.com>
2018-04-14 11:51:58 +0530

    Solved user info git warning

--- app/src/main/java/com/udacity/nanodegree/blooddonation/ui/registration/UserRegistrationInfo.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/ui/registration/UserRegistrationInfo.java
@@ -5,53 +5,47 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 
-
 /**
  * Created by riteshksingh on Apr, 2018
  */
 public class UserRegistrationInfo {
-    public ObservableField<String> phoneNumber =
-            new ObservableField<>();
+  public ObservableField<String> phoneNumber = new ObservableField<>();
 
-    public ObservableField<String> otp =
-        new ObservableField<>();
+  public ObservableField<String> otp = new ObservableField<>();
 
+  public ObservableField<Boolean> isCodeSent = new ObservableField<>();
 
-    public ObservableField<Boolean> isCodeSent =
-        new ObservableField<>();
+  public UserRegistrationInfo() {
+    isCodeSent.set(false);
+    phoneNumber.set("");
+    otp.set("");
+  }
 
-    public TextWatcher phoneNumberWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+  public TextWatcher phoneNumberWatcher = new TextWatcher() {
+    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        }
+    }
 
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
+    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-        }
+    }
 
-        @Override
-        public void afterTextChanged(Editable s) {
-            if (!TextUtils.equals(phoneNumber.get(), s)) {
-                phoneNumber.set(s.toString());
-            }
-        }
-    };
+    @Override public void afterTextChanged(Editable s) {
+      if (!TextUtils.equals(phoneNumber.get(), s)) {
+        phoneNumber.set(s.toString());
+      }
+    }
+  };
 
   public TextWatcher otpWatcher = new TextWatcher() {
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
 
     }
 
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
+    @Override public void afterTextChanged(Editable s) {
       if (!TextUtils.equals(otp.get(), s)) {
         otp.set(s.toString());
       }
