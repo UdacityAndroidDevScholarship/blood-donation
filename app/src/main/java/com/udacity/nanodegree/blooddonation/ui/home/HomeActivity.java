0482998530c50ae411c8384b69d2085ec6d261db
tavishjain <jaintavish@yahoo.com>
2018-04-14 23:41:29 +0530

    Added the feature to log out of the app
    Added the feature to log out of the app. On signing out , the user will reach the login screen.

--- app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomeActivity.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.udacity.nanodegree.blooddonation.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.udacity.nanodegree.blooddonation.R;
 import com.udacity.nanodegree.blooddonation.base.BaseActivity;
+import com.udacity.nanodegree.blooddonation.ui.login.view.UserLoginActivity;
 
 /**
  * Created by riteshksingh on Apr, 2018
@@ -15,4 +20,25 @@ public class HomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch(itemId){
+            case R.id.menu_sign_out :
+                FirebaseAuth.getInstance().signOut();
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    Intent intent = new Intent(HomeActivity.this, UserLoginActivity.class);
+                    startActivity(intent);
+                }
+        }
+        return true;
+    }
+
 }
