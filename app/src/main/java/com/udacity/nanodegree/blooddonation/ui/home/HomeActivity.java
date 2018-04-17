9381822df5249bf79163d9ab08332b3e56100c12
tavishjain <jaintavish@yahoo.com>
2018-04-17 23:35:16 +0530

    Add run time permission for GPS and SMS at run time. #46
    Add run time permission for GPS and SMS at run time. Couldn't find a reason to add permission for network provider

--- app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomeActivity.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.udacity.nanodegree.blooddonation.ui.home;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.udacity.nanodegree.blooddonation.R;
@@ -15,30 +18,60 @@ import com.udacity.nanodegree.blooddonation.ui.login.view.UserLoginActivity;
  * Created by riteshksingh on Apr, 2018
  */
 public class HomeActivity extends BaseActivity {
+
+    int REQUEST_CODE_LOCATION = 1 ;
+    int REQUEST_CODE_SMS = 2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        locationPermissionCheck();
+        requestSmsPermission();
+    }
+
+    //Function to check and request permission for location
+    public void locationPermissionCheck() {
+        String location_permission = Manifest.permission.ACCESS_FINE_LOCATION;
+        int grant = ContextCompat.checkSelfPermission(this,location_permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = location_permission;
+            ActivityCompat.requestPermissions(this,permission_list,REQUEST_CODE_LOCATION);
+            } else {   }
+    }
+
+    //Function to check and request permission for reading sms
+    private void requestSmsPermission() {
+        String sms_permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, sms_permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = sms_permission;
+            ActivityCompat.requestPermissions(this, permission_list, REQUEST_CODE_SMS);
+        }else {    }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main , menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        switch(itemId){
-            case R.id.menu_sign_out :
-                FirebaseAuth.getInstance().signOut();
-                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-                    Intent intent = new Intent(HomeActivity.this, UserLoginActivity.class);
-                    startActivity(intent);
-                }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_LOCATION) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Permission", "Permission: " + permissions[0] + "was " + grantResults[0]);
+                } else { }
+            }else if(requestCode == REQUEST_CODE_SMS){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Permission", "Permission: " + permissions[0] + "was " + grantResults[0]);
+            } else { }
         }
-        return true;
     }
 
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(HomeActivity.this, UserLoginActivity.class);
+            startActivity(intent);
+        }
+    }
 }
