dca37d078013ddc0694a6e2c8144edcf83682a78
Ankush Grover <ankush.dev2@gmail.com>
2018-04-25 00:35:36 +0530

    map screen update in progress

--- app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomePresenter.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/ui/home/HomePresenter.java
@@ -1,5 +1,7 @@
 package com.udacity.nanodegree.blooddonation.ui.home;
 
+import android.content.Intent;
+
 /**
  * Created by Ankush Grover(ankushgrover02@gmail.com) on 23/04/2018.
  */
@@ -14,12 +16,18 @@ public class HomePresenter implements HomeContract.Presenter {
 
     @Override
     public void onAddClicked() {
+
         view.generalInfo(0);
     }
 
     @Override
     public void onCurrentLocationClicked() {
         view.updateCamera(null);
+    }
+
+    @Override
+    public void handleActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 
     @Override
