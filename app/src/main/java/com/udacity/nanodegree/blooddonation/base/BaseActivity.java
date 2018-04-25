30a5d6f934eb05f677ff316f0313929c929bac82
Ankush Grover <ankush.dev2@gmail.com>
2018-04-25 23:52:22 +0530

    Improving Location Support (In Progress)

--- app/src/main/java/com/udacity/nanodegree/blooddonation/base/BaseActivity.java
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/base/BaseActivity.java
@@ -5,6 +5,7 @@ import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.udacity.nanodegree.blooddonation.ui.login.view.UserLoginActivity;
 
@@ -13,23 +14,25 @@ import com.udacity.nanodegree.blooddonation.ui.login.view.UserLoginActivity;
  */
 public class BaseActivity extends AppCompatActivity {
 
-  protected ViewDataBinding mBinding = null;
+    protected ViewDataBinding mBinding = null;
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override protected void onDestroy() {
-    mBinding = null;
-    super.onDestroy();
-  }
-
-  protected void logout() {
-    if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-      FirebaseAuth.getInstance().signOut();
-      Intent loginActivity = new Intent(this, UserLoginActivity.class);
-      startActivity(loginActivity);
-      finish();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-  }
+
+    @Override
+    protected void onDestroy() {
+        mBinding = null;
+        super.onDestroy();
+    }
+
+    protected void logout() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+            Intent loginActivity = new Intent(this, UserLoginActivity.class);
+            startActivity(loginActivity);
+            finish();
+        }
+    }
 }
