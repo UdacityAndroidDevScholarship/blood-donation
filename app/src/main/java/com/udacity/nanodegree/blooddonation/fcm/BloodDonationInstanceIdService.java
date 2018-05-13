072c5b3ee845257a161bb4a0b2f02bf2e4a7af6f
Ankush Grover <ankush.dev2@gmail.com>
2018-05-13 18:27:40 +0530

    Fcm in progress

---
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/fcm/BloodDonationInstanceIdService.java
@@ -0,0 +1,6 @@
+package com.udacity.nanodegree.blooddonation.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class BloodDonationInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/udacity/nanodegree/blooddonation/fcm/BloodDonationMessagingService.java
@@ -0,0 +1,6 @@
+package com.udacity.nanodegree.blooddonation.fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class BloodDonationMessagingService extends FirebaseMessagingService {
+}
